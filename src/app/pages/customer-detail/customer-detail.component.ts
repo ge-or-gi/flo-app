443dece90a6012a2085393bb8566cdb829f616c4
Georgi Mollov <gmollov@minervanetworks.com>
2020-12-03 17:05:06 +0200

    wip: cleanup code

--- src/app/pages/customer-detail/customer-detail.component.ts
+++ src/app/pages/customer-detail/customer-detail.component.ts
@@ -9,6 +9,42 @@ export class CustomerDetailComponent implements OnInit {
 
   todo = ['Fix Car', 'Buy Milk', 'Call Sam', 'Find Keys for Villa', 'Make reservation for b-day'];
 
+  suggestions = [
+    {name: 'asd12',
+  url: 'https://placeimg.com/150/150/any'},
+  {name: 'asfdsdasdf ',
+  url: 'https://placeimg.com/150/150/animals'},
+  {name: 'tut jgnv',
+  url: 'https://placeimg.com/150/150/arch'},
+  {name: 'kio fx',
+  url: 'https://placeimg.com/150/150/nature'},
+  {name: 'ddfdfdh',
+  url: 'https://placeimg.com/150/150/people'},
+  {name: 'poi njekn m',
+  url: 'https://placeimg.com/150/150/tech'},
+  {name: 'asde, oio',
+  url: 'https://placeimg.com/150/150/grayscale'},
+  {name: 'jashdkjahdja ',
+  url: 'https://placeimg.com/150/150/sepia'},
+  {name: '154er ir',
+  url: 'https://placeimg.com/150/150/animals'},
+  {name: 'poip 45t',
+  url: 'https://placeimg.com/150/150/arch'},
+  {name: 'ader46 hjh',
+  url: 'https://placeimg.com/150/150/nature'},
+  {name: 'asasd 2',
+  url: 'https://placeimg.com/150/150/people'},
+  ];
+
+  user = {
+    firstname: 'Uncle',
+    lastname: 'Charlie',
+    email: 'lastname@forname.com',
+    mobile: '+359878717171',
+    suggestions: this.suggestions,
+    todo: this.todo
+  };
+
   constructor() { }
 
   ngOnInit() {
