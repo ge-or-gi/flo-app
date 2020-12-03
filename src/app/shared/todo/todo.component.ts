902f0d8d85f8d00a082d84ecb780c31c4eb6de7b
Georgi Mollov <gmollov@minervanetworks.com>
2020-12-03 14:59:08 +0200

    wip: add dummy todo component

---
+++ src/app/shared/todo/todo.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+})
+export class TodoComponent implements OnInit {
+
+  @Input() todos: Todo[];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+interface Todo {
+  todo: string;
+}
