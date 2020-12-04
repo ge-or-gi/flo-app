fc6f7b5f0a768e0c6205d21b011e2f8a17dcf493
Georgi Mollov <gmollov@minervanetworks.com>
2020-12-04 10:18:32 +0200

    wip: account details component

--- src/app/pages/account-details/account-details.module.ts
+++ src/app/pages/account-details/account-details.module.ts
@@ -4,10 +4,13 @@ import { CommonModule } from '@angular/common';
 import { AccountDetailsRoutingModule } from './account-details-routing.module';
 import { AccountDetailsComponent } from './account-details.component';
 import { SharedModule } from '../../shared/shared.module';
+import { AccountHistoryComponent } from './account-history/account-history.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { AccountFeedbackComponent } from './account-feedback/account-feedback.component';
 
 
 @NgModule({
-  declarations: [AccountDetailsComponent],
+  declarations: [AccountDetailsComponent, AccountHistoryComponent, AccountSettingsComponent, AccountFeedbackComponent],
   imports: [
     CommonModule,
     AccountDetailsRoutingModule,
