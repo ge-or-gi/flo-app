30227b023e0a075d2fc0f57a9e8abb828f35bc79
Georgi Mollov <gmollov@minervanetworks.com>
2020-12-04 10:19:38 +0200

    wip: adding modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,12 +4,37 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { GridComponent } from './grid/grid.component';
 import { TodoComponent } from './todo/todo.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
+
+
 
 const components = [
   GridComponent,
   TodoComponent];
+
 const modules = [
-  CommonModule
+  CommonModule,
+  MatSidenavModule,
+  MatListModule,
+  MatGridListModule,
+  MatToolbarModule,
+  MatSlideToggleModule,
+  MatCardModule,
+  MatTableModule,
+  MatInputModule,
+  MatIconModule,
+  MatButtonModule
 ];
 
 @NgModule({
